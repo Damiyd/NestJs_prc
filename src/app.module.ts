3bf74f1c7bf817aa2d6d10d925685e6a3e9c7459
Damiyd <dpekfa144@gmail.com>
2023-01-13 14:48:35 +0900

    feat: app.service,controller  삭제 및 movies controller 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 @Module({ // 데코레이터 라고 말함 // 데코레이터는 클래스에 함수 기능을 추가할 수 있다.
   imports: [],
-  controllers: [AppController], // url 을 가져오고 함수를 실행하는것  // express의 라우터 같은 존재
-  providers: [AppService],  // 
+  controllers: [MoviesController], // url 을 가져오고 함수를 실행하는것  // express의 라우터 같은 존재
+  providers: [],  // 
 })
 export class AppModule {}
