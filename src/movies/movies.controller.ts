b1e67ccff27e8eff1791b8b39e82d6134bc23cf1
Damiyd <dpekfa144@gmail.com>
2023-01-13 15:42:27 +0900

    feat: movie.controller CRUD 추가

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,4 +7,25 @@ export class MoviesController {
     getAll() {
         return "This will return all movies";
     }
+
+    @Get("/:id")    // url 이름과 밑에 Param 이름은 같아야함 하지만 뒤에 타입 지정으로 나오는 이름은 달라도 됨
+    getOne(@Param("id") moveiId:string) {
+        return `This will return one movie with the id: ${moveiId}`;
+    }
+
+    @Post()
+    create() {
+        return "This will create a movie";
+    }
+
+    @Delete("/:id")
+    remove(@Param("id") movieId:string) {
+        return `This will delete a movie with the id: ${movieId}`;
+    }
+
+    @Patch("/:id")
+    patch(@Param("id") movieId:string) {
+        return `This will patch a movie with the id: ${movieId}`;
+    }
+    
 }
