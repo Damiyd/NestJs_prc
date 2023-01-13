be2aba1e122d230e88b59c8bfcc1d04aee23ca06
Damiyd <dpekfa144@gmail.com>
2023-01-13 13:54:30 +0900

    fix: 주석추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-@Module({
+@Module({ // 데코레이터 라고 말함 // 데코레이터는 클래스에 함수 기능을 추가할 수 있다.
   imports: [],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);  // app 모듈은 route 모듈 같은 것이다.
   await app.listen(3000);
 }
 bootstrap();
