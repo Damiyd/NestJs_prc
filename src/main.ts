83fecd06034516ea8e9bf5d773860168a8c1034e
Damiyd <dpekfa144@gmail.com>
2023-01-13 16:56:17 +0900

    feat: dto 추가 벨리데이션 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);  // app 모듈은 route 모듈 같은 것이다.
+  app.useGlobalPipes(new ValidationPipe()); // pipe는 미들웨어 같은 느낌이다.
   await app.listen(3000);
 }
 bootstrap();
