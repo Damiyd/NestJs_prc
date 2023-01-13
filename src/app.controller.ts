a48ab8983a0b873567a4f2544f7dd6c6d40b23c3
Damiyd <dpekfa144@gmail.com>
2023-01-13 14:20:45 +0900

    feat: app.service  getHi 함수 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,8 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get("/hello")
-  sayHello(): string {
-    return "Hello everyone";
+  @Get("/hello")  // 데코레이터는 꾸며주는 함수나 클래스랑 붙어있어야 한다. // 그러니 밑에 빈칸을 두면 안된다.
+  sayHello(): string {  // NestJs는 라우터 세팅하지 않아도 됨 굳;
+    return this.appService.getHi()
   }
 }
