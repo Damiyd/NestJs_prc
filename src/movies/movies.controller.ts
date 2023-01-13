6ef9857f95f583eae24995a7084f80df4ce57a94
Damiyd <dpekfa144@gmail.com>
2023-01-13 16:42:10 +0900

    feat: moive.service CRUD 추가

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,40 +1,35 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
 
+    constructor(private readonly moivesService: MoviesService) {}
+
     @Get()
-    getAll() {
-        return "This will return all movies";
+    getAll(): Movie[] {
+        return this.moivesService.getAll();
     }    
     
     @Get(":id")    // url 이름과 밑에 Param 이름은 같아야함 하지만 뒤에 타입 지정으로 나오는 이름은 달라도 됨
-    getOne(@Param("id") moveiId:string) {
-        return `This will return one movie with the id: ${moveiId}`;
-    }
-   
-    @Get("sarch")
-    search(@Query("year") searchingYear:string) {
-        return `We are searching for a movie made after: ${searchingYear}`;
+    getOne(@Param("id") movieId:string): Movie {
+        return this.moivesService.getOne(movieId)
     }
 
     @Post()
     create(@Body() movieData) {
-        console.log(movieData)
-        return movieData;
+        return this.moivesService.create(movieData);
     }
 
     @Delete(":id")
     remove(@Param("id") movieId:string) {
-        return `This will delete a movie with the id: ${movieId}`;
+        return this.moivesService.deleteOne(movieId);
     }
 
     @Patch(":id")
     patch(@Param("id") movieId:string, @Body() updateData) {
-        return {
-            updateDatadMoive: movieId,
-            ...updateData,
-        };
+        return this.moivesService.update(movieId, updateData);
     }
 
 
